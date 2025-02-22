8f275859404b385c4bf73a6500bfe3284e9339b8
Fonyuy Gita <FonyuyJudegita@gmail.com>
2025-02-21 19:00:21 -0800

    working and setting up project templates for next 15

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,14 @@
+import React, { ReactNode } from 'react'
+
+const layout = ({ children }: { children: ReactNode }) => {
+    return (
+        <main className='root-container'>
+            <div className="mx-auto max-w-7xl">
+                Header
+            </div>
+            {children}
+        </main>
+    )
+}
+
+export default layout
