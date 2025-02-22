09caa0dff88d1a51d069c681005b31f5a97b04e4
Fonyuy Gita <FonyuyJudegita@gmail.com>
2025-02-21 19:28:28 -0800

    a

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,13 +1,33 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import BookList from "@/components/BookList";
+import BookOverview from "@/components/BookOverview";
+import Header from "@/components/Header";
+import { sampleBooks } from "@/constants";
+// import { db } from "@/database/drizzle";
+// import { books, users } from "@/database/schema";
+// import { auth } from "@/auth";
+// import { desc } from "drizzle-orm";
 
-export default function Home() {
+const Home = async () => {
+  // const session = await auth();
+
+  // const latestBooks = (await db
+  //   .select()
+  //   .from(books)
+  //   .limit(10)
+  //   .orderBy(desc(books.createdAt))) as Book[];
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1>Hello Library</h1>
-      <p>Lorem, ipsum dolor.</p>
-      <Button>Click me</Button>
+    <>
 
-    </div>
+      {/* <BookOverview {...sampleBooks}  /> */}
+
+      {/* <BookList
+        title="Latest Books"
+        books={latestBooks.slice(1)}
+        containerClassName="mt-28"
+      /> */}
+    </>
   );
-}
+};
+
+export default Home;
