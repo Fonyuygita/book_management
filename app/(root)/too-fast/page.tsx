78287b9e6962cfe2999476a8ed3acda7af5edab5
Fonyuy Gita <FonyuyJudegita@gmail.com>
2025-03-18 23:17:00 -0700

    started with workflows

---
+++ app/(root)/too-fast/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+    return (
+        <main className='root-container flex min-h-screen flex-col items-center justify-center'>
+            <h1 className='font-bebas-neue text-5xl font-bold text-light-100'>Too many request-please slow down</h1>
+            <p>Making too many request to this endpoint, please slow down your speed</p>
+        </main>
+    )
+}
+
+export default page
