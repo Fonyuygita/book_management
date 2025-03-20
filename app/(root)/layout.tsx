548f158086fbed71cd213f339b9e11afb9e143ef
Fonyuy Gita <FonyuyJudegita@gmail.com>
2025-03-19 20:56:27 -0700

    added protection of routes

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,9 +1,11 @@
 import { auth } from '@/auth'
 import Header from '@/components/Header'
+import { redirect } from 'next/navigation'
 import React, { ReactNode } from 'react'
 
 const layout = async ({ children }: { children: ReactNode }) => {
     const session = await auth()
+    if (!session) redirect('/sign-in')
     console.log(session)
     return (
         <main className='root-container'>
