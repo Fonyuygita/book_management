a47ffa56ab5c4890f1aa456fd2d7f376b2b10094
Fonyuy Gita <FonyuyJudegita@gmail.com>
2025-03-19 22:33:04 -0700

    added qstash workflow email

---
+++ app/api/workflow/onboarding/route.ts
@@ -0,0 +1,52 @@
+import { serve } from "@upstash/workflow/nextjs";
+
+type InitialData = {
+  email: string;
+};
+
+export const { POST } = serve<InitialData>(async (context) => {
+  const { email } = context.requestPayload;
+  // send email, when new user signup
+
+  await context.run("new-signup", async () => {
+    await sendEmail("Welcome to the platform", email);
+  });
+  // Wait 3 days
+
+  await context.sleep("wait-for-3-days", 60 * 60 * 24 * 3);
+
+  while (true) {
+    const state = await context.run("check-user-state", async () => {
+      return await getUserState();
+    });
+
+    // OR email, non active users
+
+    if (state === "non-active") {
+      await context.run("send-email-non-active", async () => {
+        await sendEmail("Email to non-active users", email);
+      });
+      // send emails
+    } else if (state === "active") {
+      await context.run("send-email-active", async () => {
+        await sendEmail("Send newsletter to active users", email);
+      });
+    }
+
+    await context.sleep("wait-for-1-month", 60 * 60 * 24 * 30);
+  }
+});
+
+async function sendEmail(message: string, email: string) {
+  // Implement email sending logic here
+  console.log(`Sending ${message} email to ${email}`);
+}
+
+type UserState = "non-active" | "active";
+
+const getUserState = async (): Promise<UserState> => {
+  // Implement user state logic here
+  return "non-active";
+};
+
+// QSTASH ALLOWS US TO TRIGGER THE WORKFLOW, BUT WE HAVE TO USE ANOTHER TOOL, TO ACTUALLY SEND OUT THE EMAILS
--- lib/workflow.ts
+++ lib/workflow.ts
@@ -2,9 +2,40 @@
 
 import config from "@/config";
 import { Client as WorkflowClient } from "@upstash/workflow";
+
+import { Client as QStashClient, resend } from "@upstash/qstash";
 // import { config } from 'dotenv'
 // import { config } from 'dotenv'
 export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
+
+const qstashClient = new QStashClient({
+  token: config.env.upstash.qstashToken,
+});
+
+// Create a functionality to send out an email
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "IWS  Acme <onboarding@resend.dev>",
+      to: ["delivered@resend.dev"],
+      subject: subject,
+      html: message,
+    },
+  });
+};
