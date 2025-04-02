fc53001cbd5829a57c78797a7e355c694b4e844f
Fonyuy Gita <FonyuyJudegita@gmail.com>
2025-04-02 04:24:25 -0700

    added stuffs

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,19 +2,19 @@ import BookList from "@/components/BookList";
 import BookOverview from "@/components/BookOverview";
 import Header from "@/components/Header";
 import { sampleBooks } from "@/constants";
-// import { db } from "@/database/drizzle";
-// import { books, users } from "@/database/schema";
-// import { auth } from "@/auth";
-// import { desc } from "drizzle-orm";
+import { db } from "@/database/drizzle";
+import { books, users } from "@/database/schema";
+import { auth } from "@/auth";
+import { desc } from "drizzle-orm";
 
 const Home = async () => {
-  // const session = await auth();
-
-  // const latestBooks = (await db
-  //   .select()
-  //   .from(books)
-  //   .limit(10)
-  //   .orderBy(desc(books.createdAt))) as Book[];
+  const session = await auth();
+  // getting data from our neon d using drizzle
+  const latestBooks = (await db
+    .select()
+    .from(books)
+    .limit(10)
+    .orderBy(desc(books.createdAt))) as Book[];
 
   return (
     <>
